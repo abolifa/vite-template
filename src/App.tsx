674a2d8247afb74e8bdc13cf1007632bbcb303ed
abolifa <abulifa@outlook.com>
2025-11-29 18:40:06 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Home, Layout } from "lucide-react";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
